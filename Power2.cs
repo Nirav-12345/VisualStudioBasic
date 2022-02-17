94d03aa9052bbccac71bbe0f301bae7c4e2dc4c2
Nirav-12345 <Nirav-12345>
2022-02-17 09:59:01 -0800

    adding power program file

---
+++ Power2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Power_2
+{
+    public class Power2
+    {
+        public static void power()
+        {
+            int NUM;
+            Console.WriteLine("Enter Year");
+            NUM = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            if ((NUM % 4 == 0 && NUM % 100 != 0) || (NUM % 400 == 0))
+            {
+                Console.WriteLine("It is a leap year");
+            }
+
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year");
+            }
+
+        }
+    }
+}
