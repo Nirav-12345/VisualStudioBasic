42eb6358657f5b54553c8c6c1a7a08cee0a13990
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-17 07:54:04 -0800

    Leap yaer class

---
+++ LeapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    public class LeapYear
+    {
+
+        public static void Leapyear()
+
+        {
+            int NUM;
+            Console.WriteLine("Enter Year");
+            NUM = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            if ((NUM % 4 == 0 && NUM % 100 != 0) || (NUM % 400 == 0))
+            {
+                Console.WriteLine("It is a leap year");
+            }
+
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year");
+            }
+
+
+        }
+    }
+}
