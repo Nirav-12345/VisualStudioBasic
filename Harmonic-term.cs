e3b63972b3b7067d22624c1c3592cee94afe8d69
Nirav-12345 <Nirav-12345>
2022-02-17 09:56:46 -0800

    adding harmonic program

---
+++ Harmonic-term.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonic_term
+{
+    public class Harmonic_term
+
+
+    {
+        public static void Harmonic()
+            {
+
+
+            double n = Convert.ToInt32(Console.ReadLine());
+
+            double harmonic = 1;
+
+            for (float i = 2; i <= n; i++)
+
+            {
+                harmonic = harmonic + 1 / i;
+
+                Console.WriteLine(harmonic);
+
+
+            }
+
+        }
+}
+}
