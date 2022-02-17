770c505f8145976cbfc3e1ae8a3ef1390a4f3c0d
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-17 08:25:38 -0800

    adding class file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FlipsCoin
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please enter the flips");
+
+
+            int flips1 = Convert.ToInt32(Console.ReadLine());
+           double val= CoinFlepes.coin1(flips1,2);
+            Console.WriteLine("head percentage " + val);
+
+        }
+    }
+
+}
